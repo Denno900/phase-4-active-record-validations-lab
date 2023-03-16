2b3f12941d54bc7cb62f8c0257a60b795c25ef40
dennis mwaniki <dennomaz@gmail.com>
2023-03-16 20:15:40 +0300

    assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        click_baits = ["Won't Beleive", "Secret", "Top [number]", "Guess"]
+
+        unless click_baits.include?(title)
+            errors.add(:title, "It is not clickbaity enough")
+        end 
+    end
 end
